7db838470b81880bed89e1666ebab2a30a6e2681
sergii-onyx <twilight.cympak@gmail.com>
2022-03-19 20:01:46 -0400

    change i little html, add save func and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 var currentDay = document.querySelector("#currentDay");
+const indexConvertToTime = 8;
+var index;
+var tasks = [];
 
-
-
+//
 currentDay.textContent = "Today is " + moment().format('MMMM Do YYYY, HH:mm');
 
-tasks = [];
 // EDIT <p> by changing to <textarea> 
-$(".time-block").on("click", function() {
+$(".col-10").on("click", function() {
     var text = $(this).find("p")
     .text()
     .trim();
@@ -21,7 +22,7 @@ $(".time-block").on("click", function() {
 });
 
 // GET text from <textarea> and put in new created <p>
-$(".time-block").on("blur","textarea",function(){
+$(".col-10").on("blur","textarea",function(){
     // get the textarea's current text
     var text = $(this)
     .val()
@@ -30,20 +31,52 @@ $(".time-block").on("blur","textarea",function(){
         text = "";
     };
     // get index
-    var index = $(this).closest("div").attr("id");
+    index = $(this).closest("div").attr("id");
     index = JSON.parse(index);
     //console.log(index);
     tasks[index] = text;
+    //saveTask();
     // create and fill <p> element
-    var taskP = $('<p class="col-10">')
+    var taskP = $('<p>')
     .text(text);
     
     // replace <textarea> with <p> element
     $(this).replaceWith(taskP);
 
+    // Pass task's p element into auditTask() to check due time
+    checkTask($(taskP));
 });
 
+function newOne (params) {
+    console.log();
+}
 
+// CHECK TASK AND CHANGE THE CLASS
+var checkTask = function(taskEl) {
+    //convert index into time of time-block
+    time = index + indexConvertToTime;
+    var currentTime = moment().hour();
 
+    // remove any old classes from parent div
+    $(taskEl).closest("div").removeClass("past present future");
 
+    // add new class
+    if (currentTime > time) {
+        $(taskEl).closest("div").addClass("past");
+    } else if (currentTime < time) {
+        $(taskEl).closest("div").addClass("future");
+    } else if (currentTime === time) {
+        $(taskEl).closest("div").addClass("present");
+    }
+};
 
+// SAVE TASK
+var saveTask = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// SAVE BUTTON
+$(".saveBtn").on("click", function() {
+    saveTask();
+    checkTask();
+});
